b517d28d3b070acddb2b34c7b70e74ca864ce3ef
Ishara125 <IT23824874@my.sliit.lk>
2025-08-12 14:54:37 +0530

    updated admin page

--- src/pages/admin.jsx
+++ src/pages/admin.jsx
@@ -1,23 +1,27 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Link } from "react-router-dom";
+import { FaUserLarge } from "react-icons/fa6";
+import { FaWarehouse } from "react-icons/fa6";
+import { MdOutlineFavoriteBorder } from "react-icons/md";
 
-export default function Adminpage(){
-    return(
-        <div className="w-full h-screen bg-gray-300 flex p-2">
-            <div className="w-[300px] h-full flex flex-col text-center items-center ">
-                <a href="/admin" className="w-[300px] h-[50px] bg-green-500 text-white rounded-xl cursor-pointer">Dashboard</a>
-                <a href="/admin/users" className="w-[300px] h-[50px] bg-green-500 text-white rounded-xl cursor-pointer">users</a>
-                <a href="/admin/products" className="w-[300px] h-[50px] bg-green-500 text-white rounded-xl cursor-pointer">Products</a>
-                <a href="/admin/orders" className="w-[300px] h-[50px] bg-green-500 text-white rounded-xl cursor-pointer">Orders</a>
-            </div>
-            <div className="h-full bg-white w-[calc(100vw-300px)]">
-                <Routes path="/*">
-                    <Route path="/users" element={<h1>users</h1>}></Route>
-                    <Route path="/products" element={<h1>Products</h1>}></Route>
-                    <Route path="/orders" element={<h1>Orders</h1>}></Route>
-                </Routes>
-            </div>
+export default function Adminpage() {
+  return (
+    <div className="w-full h-screen bg-gray-300 flex p-2">
+      {/* Sidebar */}
+      <div className="w-[300px] h-full">
+        <Link to="/admin/users" className="p-2 border flex"><FaUserLarge className="mr-2"/> Users</Link>
+        <Link to="/admin/products" className="p-2 border flex"><FaWarehouse className="mr-2"/>Products</Link>
+        <Link to="/admin/orders" className="p-2 border flex"><MdOutlineFavoriteBorder className="mr-2" />Orders</Link>
+      </div>
 
-        </div>
-    )
-
+      {/* Main content */}
+      <div className="h-full bg-white flex-1">
+        <Routes>
+          <Route path="users" element={<h1>Users</h1>} />
+          <Route path="products" element={<h1>Products</h1>} />
+          <Route path="orders" element={<h1>Orders</h1>} />
+          <Route index element={<h1>Welcome to Admin</h1>} />
+        </Routes>
+      </div>
+    </div>
+  );
 }
